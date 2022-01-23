8b1e010589e876e1b31738d9a7128978fdcfcfe1
Wong Jia Hau <hou32hou@gmail.com>
2022-01-23 18:31:47 +0800

    feat(crkbd): added keymap wongjiahau

---
+++ keyboards/crkbd/keymaps/wongjiahau/config.h
@@ -0,0 +1,49 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#ifdef RGBLIGHT_ENABLE
+#    undef RGBLED_NUM
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLED_NUM 54
+#    undef RGBLED_SPLIT
+#    define RGBLED_SPLIT \
+        { 27, 27 }
+#    define RGBLIGHT_LIMIT_VAL 120
+#    define RGBLIGHT_HUE_STEP  10
+#    define RGBLIGHT_SAT_STEP  17
+#    define RGBLIGHT_VAL_STEP  17
+#endif
+
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+#define COMBO_TERM 30
+#define VIAL_KEYBOARD_UID {0xFF, 0x10, 0xBA, 0x45, 0x13, 0xD1, 0x84, 0x30}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
---
+++ keyboards/crkbd/keymaps/wongjiahau/rules.mk
@@ -0,0 +1,15 @@
+MOUSEKEY_ENABLE     = no     # Mouse keys
+VIA_ENABLE          = yes         # Enable VIA
+
+# Enable VIAL, refer https://get.vial.today/docs/porting-to-vial.html
+VIAL_ENABLE          = yes
+
+OLED_ENABLE = no
+OLED_DRIVER = SSD1306
+LTO_ENABLE          = yes
+COMBO_ENABLE = yes
+
+
+# Reduce firmware size, refer https://get.vial.today/docs/firmware-size.html
+LTO_ENABLE = yes
+TAP_DANCE_ENABLE = no
